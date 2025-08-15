9f959789549d1e608131f76d4c408934ae9e9869
NJ <85806608+N1nj4lxl@users.noreply.github.com>
2025-08-15 19:13:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  server: {
+    port: 5173,
+    open: true,
+  },
+  build: {
+    target: 'es2018',
+    outDir: 'dist',
+    sourcemap: false,
+    assetsInlineLimit: 0,
+  },
+});
